493ea35159ead44a45bfdc519b00937af7d673bf
Dalton Serey <daltonserey@gmail.com>
2022-04-13 23:01:11 -0300

    v0.1

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-async function main() {
-    console.log("oi!");
-    let resp1 = await fetch("http://localhost:8080/jogo/1");
-    let jogo1 = await resp1.json();
+const PRODUCTION_API = "https://dev.bolao-2022.appspot.com"
+const DEV_API = "http://0.0.0.0:8080"
+const LOCALS = ['0.0.0.0', '127.0.0.1', 'localhost'];
+const API = LOCALS.includes(location.hostname) ? DEV_API : PRODUCTION_API;
 
-    let resp2 = await fetch("http://localhost:8080/paises");
+let div = document.querySelector("div");
+
+function set_route(route) {
+    console.log(`rota: ${route}`);
+    let parts = route.split("/");
+    if (parts[1] == "jogo") {
+        view_jogo(parts[2]);
+    } else {
+        view_not_found();
+    }
+}
+
+async function view_not_found() {
+    div.innerHTML = `
+        <h1>not found!</h1>
+    `;
+}
+
+async function view_jogo(jid) {
+    let resp1 = await fetch(`${API}/jogo/${jid}`);
+    let jogo = await resp1.json();
+
+    let resp2 = await fetch(`${API}/paises`);
     let paises = await resp2.json();
 
-    console.log(jogo1);
-    let div = document.querySelector("#jogo");
-    let hora = new Date(jogo1.hora * 1000);
-    let pais1 = paises[jogo1.time1][0];
-    let band1 = paises[jogo1.time1][1];
-    let pais2 = paises[jogo1.time2][0];
-    let band2 = paises[jogo1.time2][1];
+    let hora = new Date(jogo.hora * 1000);
+    let pais1 = paises[jogo.time1][0];
+    let band1 = paises[jogo.time1][1];
+    let pais2 = paises[jogo.time2][0];
+    let band2 = paises[jogo.time2][1];
     div.innerHTML = `
-        <h2>Jogo ${jogo1.idj}</h2>
-        <p><img src="${band1}">${pais1} X ${pais2}<img src="${band2}"></p>
-        <p>${hora.toDateString()}, ${jogo1.local}</p>
+        <h2>Jogo ${jogo.id}</h2>
+        <p><img src="${band1}?tx=w_30">${pais1} X ${pais2}<img src="${band2}?tx=w_30"></p>
+        <p>${hora.toDateString()}, ${jogo.local}</p>
+        <button>p</button>
     `;
+
+    let prox = div.querySelector("button");
+    prox.addEventListener("click", function (e) {
+        let prox = `/jogo/${Number(jid) + 1}`;
+        console.log(prox);
+        set_route(prox);
+    });
+}
+
+async function main() {
+    console.log("oi!");
+    set_route(location.hash.slice(1));
 }
 
 main();
