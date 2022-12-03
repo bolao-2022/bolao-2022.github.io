18e8cc86f84bc2304b1384ec795115fed230fd31
Dalton Serey <daltonserey@gmail.com>
2022-12-03 13:36:32 -0300

    versao dev

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ const PRODUCTION_API = "https://dev-dot-bolao-2022.appspot.com"
 const DEV_API = "http://0.0.0.0:8080"
 const LOCALS = ['0.0.0.0', '127.0.0.1', 'localhost'];
 //export const API = LOCALS.includes(location.hostname) ? DEV_API : PRODUCTION_API;
-export const API = PRODUCTION_API;
+export const API = localStorage.getItem("API") || PRODUCTION_API;
 window.API = API;
+if (API != PRODUCTION_API) {
+    let estilo = 'color: blue; font-size: 20px; background: red; padding: 2px; color: white; font-weight: bold;';
+    console.log(`%cALERTA: usando ${window.API} como backend`, estilo);
+}
 
 export const FILES="https://storage.googleapis.com/bolao-2022.appspot.com/public"
 export const tabela_versao = "0-0"
